1076e66f7ba1d02258dad29b1df742746e025372
Izak <izakoberckal.plusko@gmail.com>
2024-08-08 22:43:02 +0200

    First version

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from calculations import la
 
-app = Flask(__name__)
+app = Flask(__name__)  # instantiate the Flask class
 
-with open('data/auth.txt', 'r') as file:
+with open('data/auth.txt', 'r') as file:  # read the authentication file
     content = file.read().split()
 host = content[0]
 port = content[1]
 database = content[2]
 driver = 'ODBC Driver 17 for SQL Server'
 
-connection_string = f'mssql+pyodbc://@{host}:{port}/{database}?driver={driver}&trusted_connection=yes'
-engine = create_engine(connection_string)
+connection_string = f'mssql+pyodbc://@{host}:{port}/{database}?driver={driver}&trusted_connection=yes'  # create the connection string
+engine = create_engine(connection_string)  # create the engine
 
-@app.route('/')
+@app.route('/')  # define the route
 def home():
-    data = request.args.get('data')  # penguins_size
+    data = request.args.get('data')  # penguins_size, ...
     algo = request.args.get('algo')  # knn, logreg, svm
-    query = f'SELECT * FROM ml_api.{data}'
+    format_return = request.args.get('format')  # json, xml
+
+    query = f'SELECT * FROM ml_api.{data}' # ml_api is my schema
     df = pd.read_sql(query, engine)
 
-    results = la(df, algo)
+    results = la(df, algo)  # call the la function from calculations.py
+    result_string = f'Accuracy: {results[0]:.2f}, Precision: {results[1]:.2f}, Recall: {results[2]:.2f}, F1: {results[3]:.2f}'
+
+    if format_return == 'json':
+        return jsonify({'result': result_string})
+
+    if format_return == 'xml':
+        return f'<result>{result_string}</result>'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # run the app in debug mode
+
 
--- calculations.py
+++ calculations.py
@@ -1,2 +1,80 @@
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+
 def la(df, algo):
-    pass
+
+    x = df.drop(df.columns[0], axis=1)  # features
+    y = df[df.columns[0]]  # label
+    result = []  # store the results
+
+    enc = OneHotEncoder()  # one-hot encode the features
+    x = enc.fit_transform(x).toarray()
+    sc = StandardScaler()  # scale the features
+    x = sc.fit_transform(x)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)  # split the data
+
+    if algo == 'knn':
+        knn = KNeighborsClassifier()  # instantiate the model
+
+        param_grid = {  # define the hyperparameters
+            'n_neighbors': [3, 4, 5, 6, 7, 8, 9],
+            'weights': ['uniform', 'distance'],
+            'metric': ['euclidean', 'manhattan', 'minkowski']
+        }
+
+        grid_knn = GridSearchCV(knn, param_grid, cv=5)  # instantiate the grid search
+        grid_knn.fit(x_train, y_train)  # fit the model
+        y_pred = grid_knn.predict(x_test)  # make predictions
+
+        result.append(accuracy_score(y_test, y_pred))
+        result.append(precision_score(y_test, y_pred, average='macro'))
+        result.append(recall_score(y_test, y_pred, average='macro'))
+        result.append(f1_score(y_test, y_pred, average='macro'))
+
+        return result
+
+    if algo == 'logreg':
+        logreg = LogisticRegression()  # instantiate the model
+
+        param_grid = {  # define the hyperparameters
+            'C': [0.1, 1, 10, 100],
+            'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
+        }
+
+        grid_logreg = GridSearchCV(logreg, param_grid, cv=5)  # instantiate the grid search
+        grid_logreg.fit(x_train, y_train)  # fit the model
+        y_pred = grid_logreg.predict(x_test)  # make predictions
+
+        result.append(accuracy_score(y_test, y_pred))
+        result.append(precision_score(y_test, y_pred, average='macro'))
+        result.append(recall_score(y_test, y_pred, average='macro'))
+        result.append(f1_score(y_test, y_pred, average='macro'))
+
+        return result
+
+    if algo == 'svm':
+        svm = SVC()  # instantiate the model
+
+        param_grid = {  # define the hyperparameters
+            'C': [0.1, 1, 10, 100],
+            'gamma': [1, 0.1, 0.01, 0.001],
+            'kernel': ['rbf', 'linear']
+        }
+
+        grid_svm = GridSearchCV(svm, param_grid, cv=5)  # instantiate the grid search
+        grid_svm.fit(x_train, y_train)  # fit the model
+        y_pred = grid_svm.predict(x_test)  # make predictions
+
+        result.append(accuracy_score(y_test, y_pred))
+        result.append(precision_score(y_test, y_pred, average='macro'))
+        result.append(recall_score(y_test, y_pred, average='macro'))
+        result.append(f1_score(y_test, y_pred, average='macro'))
+
+        return result
